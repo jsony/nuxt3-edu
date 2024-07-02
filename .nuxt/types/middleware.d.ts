e3cf59ae9dc888f445d88d6ef678c09b44e6f472
Jason <jsonm163@gmail.com>
2024-07-02 17:27:59 +0800

    t2222240702

--- .nuxt/types/middleware.d.ts
+++ .nuxt/types/middleware.d.ts
@@ -1,6 +1,6 @@
 import type { NavigationGuard } from 'vue-router'
 export type MiddlewareKey = "auth" | "createorder" | "list" | "only-visitor" | "search"
-declare module "E:/WebProject/nuxt3-edu/node_modules/nuxt/dist/pages/runtime/composables" {
+declare module "D:/webdir/git_jasonsn/znuxtjs/nuxt3-edu/node_modules/nuxt/dist/pages/runtime/composables" {
   interface PageMeta {
     middleware?: MiddlewareKey | NavigationGuard | Array<MiddlewareKey | NavigationGuard>
   }
